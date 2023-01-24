1fbb7029efc9c89c4c8946c411bef450dc047654
Mingyuan Ren <m28ren@uwaterloo.ca>
2023-01-24 00:33:42 -0500

    add config utils

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"gochat/router"
+	"gochat/utils"
 )
 
 func main() {
+	utils.InitConfig()
+	utils.InitMySQL()
 	r := router.Router()
 	r.Run(":8081") // listen and serve on 0.0.0.0:8080
 }
